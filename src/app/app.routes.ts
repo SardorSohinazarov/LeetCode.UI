b0f8bb36a31e21e0e80d0b63542d6fb24e64d11d
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-04-14 22:02:54 +0500

    add source files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from "./components/pages/home/home.component";
-import {NotfoundComponent} from "./components/pages/notfound/notfound.component";
+import { HomeComponent } from './components/pages/home/home.component';
+import { NotfoundComponent } from './components/pages/notfound/notfound.component';
 import { InputComponent } from './components/pages/input/input.component';
 import { ProfileComponent } from './components/pages/profile/profile.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { SignupComponent } from './components/pages/signup/signup.component';
 
 export const routes: Routes = [
-  {path: "", title: "Home page", component: HomeComponent},
-  {path: "home", title: "Home page", component: HomeComponent},
-  {path: "input", title: "Input page", component: InputComponent},
-  {path: "profile", title: "Profile page", component: ProfileComponent},
-  {path: "**", title: "Not Found", component: NotfoundComponent},
+  { path: '', title: 'Home page', component: HomeComponent },
+  { path: 'home', title: 'Home page', component: HomeComponent },
+  { path: 'input', title: 'Input page', component: InputComponent },
+  { path: 'profile', title: 'Profile page', component: ProfileComponent },
+  { path: 'login', title: 'Login page', component: LoginComponent },
+  { path: 'signup', title: 'Sign page', component: SignupComponent },
+  { path: '**', title: 'Not Found', component: NotfoundComponent },
 ];
